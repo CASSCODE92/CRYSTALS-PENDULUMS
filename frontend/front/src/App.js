eebee428946b71e0ff616073ee4df0db8d82defd
CASSCODE92 <cassie2012abioye@gmail.com>
2020-09-05 16:30:08 -0400

    NavBar List

--- frontend/front/src/App.js
+++ frontend/front/src/App.js
@@ -6,9 +6,10 @@ import NavBar from './components/NavBar';
 function App() {
   return (
     <div className="App">
-      <p>
-        Welcome to CRYSTALS & PENDULUMS
-      </p>
+     <NavBar/>
+     <Container>
+       
+     </Container>
     </div>
   );
 }
--- frontend/front/src/components/NavBar.js
+++ frontend/front/src/components/NavBar.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
-import{NavBar} from 'react-bootstrap' ;
+import{NavBar, Nav} from 'react-bootstrap' ;
 
 class NavBar extends React.Component {
   render () {
     return (
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="/"><img src="%PUBLIC_URL%/favicon.ico" width="25" height="25" alt="brand"/> CRYSTALS & PENDULUMS</Navbar.Brand>
+        <Navbar.Brand href="/">
+            <img src="%PUBLIC_URL%/favicon.ico" width="25" height="25" alt="brand"/> CRYSTALS & PENDULUMS</Navbar.Brand>
+            </NavBar.Brand>
+            <Nav className="pages">
+            <Nav.Link href="#">Item Upload</Nav.Link>
+            <Nav.Link href="#">Crystals</Nav.Link>
+            <Nav.Link href="#">Jewelry</Nav.Link>
+            <Nav.Link href="#">Pendulums</Nav.Link>
+          </Nav>
+        </Navbar>
 
-      </NavBar>
+
     );
 
   }
