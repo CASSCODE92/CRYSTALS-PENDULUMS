5df5d7f2b5483dafb8a2a98717aabda724259f14
CASSCODE92 <cassie2012abioye@gmail.com>
2020-09-05 15:00:13 -0400

    navbar

--- frontend/front/src/App.js
+++ frontend/front/src/App.js
@@ -1,1 +1,19 @@
-import React from 'react' ;
+import React from 'react';
+
+import{NavBar} from 'react-bootstrap' ;
+
+class NavBar extends React.Component {
+  render () {
+    return (
+      <Navbar bg="dark" variant="dark">
+
+      </NavBar>
+    );
+
+  }
+
+}
+
+export default App;
+
+
