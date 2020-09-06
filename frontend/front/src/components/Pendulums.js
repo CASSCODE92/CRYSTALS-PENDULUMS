9935286f2bdc5f0a264d493728cb4f7ef5a927d2
CASSCODE92 <cassie2012abioye@gmail.com>
2020-09-06 00:16:17 -0400

    Pendulum.js

---
+++ frontend/front/src/components/Pendulums.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Pendulum extends React.Component {
+    render () {
+        return (<div className= "text-white">PENDULUM</div>);
+    }
+}
+
+export default Pendulum
