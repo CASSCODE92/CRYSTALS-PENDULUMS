1865cc721a16e54529a526abb36545d07b804c88
CASSCODE92 <cassie2012abioye@gmail.com>
2020-09-06 15:06:58 -0400

    App.js files Logged

--- frontend/front/src/App.js
+++ frontend/front/src/App.js
@@ -2,28 +2,37 @@ import React from 'react';
 import './App.css';
 
 import {Container, Row, Col} from 'react-bootstrap';
-
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Mission from './components/Welcome';
+import Crystals 
+import Mission from './components/Welcome';
+import Mission from './components/Welcome';
+import Mission from './components/Welcome';
+import Jewelry from './components/Jewelry';
 
 function App() {
   const marginTop = {
     marginTop:"20px"
   };
   return (
-    <div className="App">
+    <Router>
      <NavBar/>
      <Container>
        <Row>
          <Col lg={12} style={marginTop}>
-           <Mission/>
+           <Crystals/>
+           <Jewelry/>
+           <Pendulums/>
+           <ItemUpload/>
          </Col>
        
 
 
        </Row>
      </Container>
-    </div>
+     <Footer/>
+    </Router>
   );
 }
 
