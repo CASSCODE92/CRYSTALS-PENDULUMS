d1113a5969bdc7488449f18090ee453888f6362d
CASSCODE92 <cassie2012abioye@gmail.com>
2020-09-06 00:13:58 -0400

    Jewelry.js

---
+++ frontend/front/src/components/Jewelry.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Jewelry extends React.Component {
+    render () {
+        return (<div className= "text-white">JEWELRY</div>);
+    }
+}
+
+export default Jewelry
