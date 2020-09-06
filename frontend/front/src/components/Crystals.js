a584cbc7a4d995597e5f18f78493d60420cca05f
CASSCODE92 <cassie2012abioye@gmail.com>
2020-09-06 00:07:10 -0400

    Crystals.js setup

---
+++ frontend/front/src/components/Crystals.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Crystals extends React.Component {
+    render () {
+        return (<div className= "text-white">CRYSTALS</div>);
+    }
+}
+
+export default Crystals
