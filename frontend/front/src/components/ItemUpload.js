33734b076f1e43bec73c150feb3508c8dd8205d4
CASSCODE92 <cassie2012abioye@gmail.com>
2020-09-06 00:17:52 -0400

    ItemUpload.js setup

---
+++ frontend/front/src/components/ItemUpload.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class ItemUpload extends React.Component {
+    render () {
+        return (<div className= "text-white">ITEM UPLOAD</div>);
+    }
+}
+
+export default ItemUpload
